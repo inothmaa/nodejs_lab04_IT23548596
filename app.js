a4b6026c773c0770af71ba491bd5cd9a924afa22
Arani Inothma <inothmaa@gmail.com>
2026-02-18 12:45:23 +0530

    display the txt file content(hello node).

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log("Node.js lab started successfully");
+const fs = require('fs');
+
+fs.readFile('file.txt', 'utf8', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
