228cf9b64448995c3df67d1112e0c8b4168c113c
Arani Inothma <inothmaa@gmail.com>
2026-02-18 12:50:51 +0530

    File saved(Hello world).

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 console.log("Node.js lab started successfully");
 const fs = require('fs');
 
+//read - file
 fs.readFile('file.txt', 'utf8', function (err, data) {
   if (err) throw err;
   console.log(data);
+});
+
+//write -file
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+  if (err) throw err;
+  console.log('File saved!');
 });
